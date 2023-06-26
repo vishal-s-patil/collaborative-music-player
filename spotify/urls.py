6e4642bc15ecd97f273ebb980f23d02979faf182
vishalspatil <vishalspolicepatil55@gmail.com>
2023-06-27 02:36:19 +0530

    current song spotify

--- spotify/urls.py
+++ spotify/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('get-auth-url', views.AuthURL.as_view()),
     path('redirect', views.spotify_callback),
-    path('is-authenticated', views.IsAuthenticated.as_view())
+    path('is-authenticated', views.IsAuthenticated.as_view()),
+    path('current-song', views.CurrentSong.as_view()),
+    path('spotify-tokens', views.SpotifyModelView.as_view()),
+    path('clear-spotify-tokens', views.ClearSpotifyTokens.as_view()),
 ]
