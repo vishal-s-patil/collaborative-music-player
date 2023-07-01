5dff458065332167390539b06a9d35f3ace5f209
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2023-07-02 00:18:21 +0530

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID = "f2cb36a46cd242ab95df1e5c8f3840df"
-CLIENT_SECRET = "1800cc8c67d646ab8509e5d8ab4fd06e"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
